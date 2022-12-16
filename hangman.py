4d5c86222d83d9b0c138b098beb2da168201e9c2
Dean Pierrot <dapierrot21@gmail.com>
2022-12-16 10:23:43 -0500

    File setup

---
+++ hangman.py
@@ -0,0 +1,11 @@
+class Hangman:
+    # Creates random word
+    # Checks the string from the player and see if it's part of the random word
+    # List of a stick figure to use as a way to show the player how many tries the player has left yo guess the word correctly.
+    pass
+
+
+class Player:
+    # Obtains audio from user microphone
+    # Use google speech recognition on the audio file and convert to text
+    pass
