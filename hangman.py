4ff29812dc9de5c91ea405496a33aa11ba37f795
Dean Pierrot <dapierrot21@gmail.com>
2022-12-17 18:53:51 -0500

    implemented pyttsx3 speak() to speak out to a the player a given string.

--- hangman.py
+++ hangman.py
@@ -1,11 +1,37 @@
+import pyttsx3
+
+
 class Hangman:
-    # Creates random word
-    # Checks the string from the player and see if it's part of the random word
+
     # List of a stick figure to use as a way to show the player how many tries the player has left yo guess the word correctly.
-    pass
+    def __init__(self):
+        # This is where the stick figure will be added too.
+        self.wrong_guesses = []
+
+    # Creates random word
+    def create_random_word():
+        pass
+
+    # Checks the string from the player and see if it's part of the random word
+    def check_player_letter(self, letter):
+        pass
 
 
 class Player:
+
+    engine = pyttsx3.init()
+    engine.setProperty('rate', 65)
+
     # Obtains audio from user microphone
     # Use google speech recognition on the audio file and convert to text
-    pass
+    def __init__(self, name):
+        self.name = name
+
+    def speak(self, text):
+        self.engine.say(text)
+        self.engine.runAndWait()
+
+
+test = Player('Dean')
+
+test.speak("Hi {player}! welcome to speech to text hangman console game. To play this game you will need to make sure your microphone is on. Using the microphone you will speak your letter to guess the word. Good luck!".format(player=test.name))
