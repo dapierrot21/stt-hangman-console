3194b227e88b3c7ff746c5ff51948e6c4c38f5a1
Dean Pierrot <dapierrot21@gmail.com>
2022-12-19 16:45:10 -0500

    Implemented a function to greet the player with the correct time of the day, also a function to take the player's statement but the only issue with this is that the player must say certain letters as words with that letter as the first letter in the word. Because letters like u comes out as you and c comes out as see.

--- hangman.py
+++ hangman.py
@@ -1,12 +1,13 @@
+import datetime
+import speech_recognition as sr  # speech recogniton.
+import os
 import pyttsx3
 from random_word import RandomWords
 
 
 class Hangman:
 
-    # List of a stick figure to use as a way to show the player how many tries the player has left yo guess the word correctly.
     def __init__(self):
-        # This is where the stick figure will be added too.
         self.wrong_guesses = []
         self.correct_letters = []
         self.random_word = self.create_random_word()
@@ -15,14 +16,10 @@ class Hangman:
             [] for word in self.random_word]
 
     # Creates random word
-
     def create_random_word(self):
         r = RandomWords()
         word = r.get_random_word()
-        # Return a single random word
         return word
-
-    # Checks the string from the player and see if it's part of the random word
 
     def check_player_letter(self, player_guess):
 
@@ -33,20 +30,53 @@ class Hangman:
             self.place_holder_for_correct_letters[index] += player_guess
             self.correct_letters.append(player_guess)
 
+    def greeting_of_the_day(self, name):
+        hour = datetime.datetime.now().hour
+        if hour >= 0 and hour < 12:
+            print("Hello,Good Morning {player_name}!".format(
+                player_name=name))
+        elif hour >= 12 and hour < 18:
+            print("Hello,Good Afternoon {player_name}!".format(
+                player_name=name))
+        else:
+            Player.speak("Hello,Good Evening {player_name}!".format(
+                player_name=name))
+            print("Hello,Good Evening {player_name}!".format(
+                player_name=name))
+
 
 class Player:
-
     engine = pyttsx3.init()
-    engine.setProperty('rate', 65)
+    engine.setProperty('rate', 75)
 
-    # Obtains audio from user microphone
-    # Use google speech recognition on the audio file and convert to text
     def __init__(self, name):
         self.name = name
 
     def speak(self, text):
         self.engine.say(text)
         self.engine.runAndWait()
+
+    def takeCommand(self):
+        r = sr.Recognizer()
+        with sr.Microphone() as source:
+            print("If you can not get your letter recognized then try saying a word with that letter as the first letter in the word. Ex: for the letter \"c\" try saying cat.")
+            self.speak("I am now able to take your letter you want to try.")
+            print("I am now able to take your letter you want to try.")
+            # Recommended for situations where the ambient noise level is unpredictable, which seems to be the majority of use cases. If the ambient noise level is strictly controlled, better results might be achieved by setting this to False to turn it off.
+            r.dynamic_energy_threshold = True
+            r.adjust_for_ambient_noise(source, duration=1)
+            r.pause_threshold = 1
+            audio = r.listen(source)
+            statement = ""
+            try:
+                statement = r.recognize_google(audio, language='en-US').lower()
+                print(f"You said: {statement[0]}\n")
+
+            except sr.UnknownValueError:
+                print("Sorry I could not hear you.")
+            except sr.RequestError as e:
+                print("Request Failed; {0}".format(e))
+            return statement[0]
 
 
 test_speak = Player('Dean')
@@ -57,10 +87,8 @@ test_random_word = Hangman()
 # print(test_random_word.create_random_word())
 
 
-test_random_word.check_player_letter("e")
-test_random_word.check_player_letter("o")
-print(test_random_word.random_word)
-print(test_random_word.place_holder_for_correct_letters)
-
+test_random_word.greeting_of_the_day(test_speak.name)
+letter_to_try = test_speak.takeCommand()
+test_random_word.check_player_letter(letter_to_try)
 
 print(test_random_word.place_holder_for_correct_letters)
